b539c3a601140264e09ac0f83d60fdaae8c4eba5
Ankush <ankush357159@gmail.com>
2022-06-13 13:30:50 +0530

    react query

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import { HomePage } from "./components/Home.page";
+import { RQSuperHeroesPage } from "./components/RQSuperHeroes.page";
+import { SuperHeroesPage } from "./components/SuperHeroes.page";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/super-heros'>Traditional Super Heros</Link>
+            </li>
+            <li>
+              <Link to='rq-super-heros'>RQ Super Heros</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path='/super-heros' element={<SuperHeroesPage />} />
+
+          <Route path='/rq-super-heros' element={<RQSuperHeroesPage />} />
+
+          <Route path='/' element={<HomePage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
